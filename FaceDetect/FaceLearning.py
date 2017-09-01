d4b8f021aecef2092a7c4c9ee3a83b0244e8d1a9
jadjer <jadjer@mail.ru>
2017-09-01 09:18:59 +0300

    Добавлена запись видео для обучения

--- FaceDetect/FaceLearning.py
+++ FaceDetect/FaceLearning.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
+import FaceDetect
 
 
 class FaceLearning:
     def __init__(self):
-        pass
+        self.fd = FaceDetect.FaceDetect()
 
     def get_move(self):
         cap = cv2.VideoCapture(0)
@@ -24,4 +25,20 @@ class FaceLearning:
 
         cap.release()
         out.release()
+        cv2.destroyAllWindows()
+
+    def learn(self, video):
+        cap = cv2.VideoCapture(video)
+        while (cap.isOpened()):
+            ret, frame = cap.read()
+            if ret == True:
+                frame = cv2.flip(frame, 90)
+                self.fd.get_descriptor(frame)
+                descr = cv2.imshow('frame', frame)
+                print(descr)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+        cap.release()
         cv2.destroyAllWindows()
